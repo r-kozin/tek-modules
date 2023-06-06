0ce805fa425bdc97f3ec9564507f12e143952eef
Ryan Kozin <ryankozin@Ryans-MacBook-Pro.local>
2023-06-06 08:43:28 -0700

    morning lecture

---
+++ app.js
@@ -0,0 +1,7 @@
+import g, { add as b, subtract } from './main.js';
+import { add as a, subtract as s } from './script.js';
+
+let person = "jim";
+console.log(g(person));
+console.log(b(4,5));
+console.log(a(4,5));
---
+++ main.js
@@ -0,0 +1,11 @@
+export function add(x,y){
+    return x+y;
+}
+
+export function subtract(x,y){
+    return x-y;
+}
+
+export default function greeting(person){
+    return 'Hello, ' + person;
+}
